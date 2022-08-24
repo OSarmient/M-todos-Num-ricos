f3477abca7e3abd8096b2c78ee6f201dc74280d5
Omar Sarmiento <omar@MacBook-Air-de-Omar.local>
2022-08-24 14:30:49 -0500

    update

--- newton.m
+++ newton.m
@@ -1,4 +1,4 @@
-function newton(f, fd, x0, n)
+function newton(f, fd, x, n)
     %NEWTON-RAPHSON
     %   input:  f -- Función a evaluar
     %           fd -- Función derivada
@@ -7,14 +7,13 @@ function newton(f, fd, x0, n)
     %   output: La impresión en pantalla de el numero de iteraciones,
     %           la diferencia el valor obtenido y el evaluado y 
     %           la evaluación de la función
-
-    x = x0;
     
     disp('          k                   pk               p(k+1)-pk             f(pk)')
 
     for i = 0:n
-        y = x - f(x)/fd(x);
+        % Ejecución iterativa de la fórmula
+        y = x - f(x)/fd(x); % y variable auxiliar para imprimir en pantalla
         disp([  i   x   y-x     f(x)])
-        x = y;
+        x = y;  % Reasignación de x
     end
 end
