7d8b97e136bd65b46558444501137bb879c9854c
semunozp <111798372+semunozp@users.noreply.github.com>
2022-09-04 12:40:03 -0500

    Update LU.m

--- Sesion_2/LU.m
+++ Sesion_2/LU.m
@@ -1,9 +1,4 @@
-function [L, U] = LU(A)
-  % LU Summary of this function goes here
-  % Input:  A -- Matriz de coeficientes del sistema
-  % Output: L -- La matriz triangular inferior
-  %         U -- La matriz triangular superior
-
+function [L U  y  x] = LU(A, B)
 
     [m,n] = size(A);                  % A tiene m filas y n columnas
     L = eye(m);                       % L es la matriz identidad de tamaño m*m
@@ -17,5 +12,37 @@ function [L, U] = LU(A)
     end
   L;                                  % Matriz triangular inferior
   U = triu(A);                        % Matriz triangular superior
-end
+
+C=[L B];     %Se le asigna a C el ensamble de la Matriz L con la matriz B
+
+  y=zeros(n,1);     %se le asigna a y una matriz vertical de n filas con valores = 0
+
+  y(1)=C(1,n+1)/C(1,1);    %Se establece la solucion para y1 para despuer generalizar el calculo
+  for i=2:n
+    s=0;
+    for j=1:n-1             %Se comienzan a correr los valores de j
+      s=s+C(i,j)*y(j);       %Acumulacion de s a medida que incrementa j
+      endfor
+    y(i)=(C(i,n+1)-s)/C(i,i);
+    endfor
+  y;
+
+  D = [U y];        %Se le asigna a D el ensamble de la Matriz U con la matriz y
+  [n, m]=size(D);
+  x=zeros(n,1);
+  x(n)=D(n,n+1)/D(n,n);
+
+  for i=n-1:-1:1
+
+    S=0;
+
+    for j=n:-1:i+1
+      S=S+D(i,j)*x(j);
+
+    endfor
+    x(i)=(D(i,n+1)-S)/D(i,i);
+  endfor
+x;
+ endfunction
+
 
